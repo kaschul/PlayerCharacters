95c3deb963c805e4d4bbf2b3bbede92f64f21fe2
kaschul <47723396+kaschul@users.noreply.github.com>
2022-03-17 13:11:12 -0700

    added header, footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
+import Footer from "./components/footer";
+import Header from "./components/header";
 
 function App() {
   return (
     <>
+      <Header />
       <h1>Character List</h1>
+      <Footer />
     </>
   );
 }

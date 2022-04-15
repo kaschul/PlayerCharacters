a7c271411cc028d58e72bc0024756ca7e2067d0f
kaschul <47723396+kaschul@users.noreply.github.com>
2022-04-15 12:45:24 -0700

    formatting

--- backend/server.js
+++ backend/server.js
@@ -17,5 +17,5 @@ app.use('/api/npcs', characterRoutes)
 app.use(notFound)
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));

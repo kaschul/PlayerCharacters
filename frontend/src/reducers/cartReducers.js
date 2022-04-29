80d3b9063bfe9a8b8eb4581e4923a0924aa5e94b
kaschul <47723396+kaschul@users.noreply.github.com>
2022-04-28 18:42:39 -0700

    unfinished - adding store components

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,13 +1,14 @@
 import {
   CART_ADD_ITEM,
-  CART_REMOVE_ITEM
+  CART_REMOVE_ITEM,
+  CART_SAVE_SENDTOEMAIL
 } from '../constants/cartConstants'
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
   switch(action.type){
     case CART_ADD_ITEM:
       const item = action.payload
-      const existItem = state.cartItems.find((x) => x.character === item.character)
+      const existItem = state.cartItems.find(((x) => x.pc === item.pc) || ((x) => x.npc === item.npc))
 
       if (existItem){
         return{
@@ -22,7 +23,7 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
     case CART_REMOVE_ITEM:
       return {
         ...state,
-        cartItems: state.cartItems.filter((x) => x.character !== action.payload)
+        cartItems: state.cartItems.filter((x) => x.pc !== action.payload)
       }
     default:
       return state
